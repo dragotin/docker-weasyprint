8271db2d3d3578edefeb331a5ca431d848b38b29
Klaas Freitag <kraft@freisturz.de>
2024-12-14 20:26:04 +0100

    Bump requirements to recent versions. Adopt wsgi startup code

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,20 @@ from weasyprint import HTML, CSS
 
 app = Flask('pdf')
 
+with app.app_context():
+    logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
+    logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
+    logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
+    logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]'
+    ))
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
+
 
 def authenticate(f):
     @wraps(f)
@@ -32,22 +46,6 @@ def auth():
 @app.route('/health')
 def index():
     return 'ok'
-
-
-@app.before_first_request
-def setup_logging():
-    logging.addLevelName(logging.DEBUG, "\033[1;36m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
-    logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
-    logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-    logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s '
-        '[in %(pathname)s:%(lineno)d]'
-    ))
-    app.logger.addHandler(handler)
-    app.logger.setLevel(logging.DEBUG)
 
 
 @app.route('/')
